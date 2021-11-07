2213da7a130bbc5e1f645052eabeb9d16de1a1ad
Yasir Uddin <yasir.uddin@icloud.com>
2021-11-06 19:24:02 -0700

    add employee functionality wired

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-console.log('JavaScript works!');
-
 $(document).ready(readyNow);
 
+let employeeInfo = [];
+
 function readyNow(){
-    console.log('jQuery works!');
     $('#submitButton').on('click', handleSubmitButton)
 }
 
 function handleSubmitButton(){
-    console.log('submit button works!');
+    let firstNameValue = $("#firstNameValue").val();
+    let lastNameValue = $("#lastNameValue").val();
+    let idValue = $("#idValue").val();
+    let jobTitleValue = $("#jobTitleValue").val();
+    let annualSalaryValue = $("#annualSalaryValue").val();
+
+    let newEmployee = {
+        firstName:firstNameValue,
+        lastName: lastNameValue,
+        ID: idValue,
+        Title: jobTitleValue,
+        Salary: annualSalaryValue
+    }
+
+    employeeInfo.push(newEmployee);
+
+    $("#firstNameValue").val('');
+    $("#lastNameValue").val('');
+    $("#idValue").val('');
+    $("#jobTitleValue").val('');
+    $("#annualSalaryValue").val('');
+
+    addEmployee(employeeInfo);
+}
+
+
+
+function addEmployee(employeeList){
+    let newRow = $('#addNewEmployeeRow')
+    newRow.empty()
+    for(let person of employeeList){
+        let newCompletedRow = `
+            <tr>
+                <td>${person.firstName}</td>
+                <td>${person.lastName}</td>
+                <td>${person.ID}</td>
+                <td>${person.Title}</td>
+                <td>${person.Salary}</td>
+            </tr>`;
+            newRow.append(newCompletedRow);
+    }
 }
